61d20d5a8912e4d0f527d84e6e89a2e463d73365
GTech9971 <98648140+GTech9971@users.noreply.github.com>
2022-11-20 23:23:32 +0900

    ビュワーのレイアウトを修正

--- ApocalypseNowApp/src/pages/Viewer/Viewer.tsx
+++ ApocalypseNowApp/src/pages/Viewer/Viewer.tsx
@@ -2,20 +2,28 @@ import {
     IonBackButton,
     IonButtons,
     IonContent,
+    IonFab,
+    IonFabButton,
+    IonGrid,
     IonHeader,
+    IonIcon,
     IonImg,
     IonPage,
     IonTitle,
     IonToolbar,
 } from '@ionic/react';
+import { checkmarkOutline } from 'ionicons/icons';
 import { memo, useEffect, useState } from 'react';
 import { RouteComponentProps } from 'react-router';
+import { useRecoilState } from 'recoil';
 import { PreviewImageService } from '../../services/PreviewImage.service';
+import { siteIdState } from '../../states/SiteId.state';
 import './Viewer.scss';
 
 const Viewer: React.FC<RouteComponentProps> = (props) => {
     const { fetchPreviewImage } = PreviewImageService();
     const [imageData, setImageData] = useState<string>();
+    const [selectSiteId, setSelectSiteId] = useRecoilState(siteIdState);
 
     useEffect(() => {
         // 2秒ごとに画像を取得
@@ -29,22 +37,43 @@ const Viewer: React.FC<RouteComponentProps> = (props) => {
         return () => { clearInterval(interval); };
     }, []);
 
+    /** サイト画像決定 */
+    const onClickDetectSiteImage = async () => {
+
+    };
+
     return (
         <IonPage>
             <IonHeader>
                 <IonToolbar>
                     <IonButtons slot="start">
-                        <IonBackButton />
+                        <IonBackButton defaultHref='/home' />
                     </IonButtons>
 
                     <IonTitle>ap2n</IonTitle>
+
+                    <IonTitle slot='end'>
+                        Site ID = {selectSiteId === undefined ? 'None' : selectSiteId}
+                    </IonTitle>
                 </IonToolbar>
             </IonHeader>
             <IonContent fullscreen>
-                {/* <img src={imageData}></img> */}
-                <IonImg src={imageData}></IonImg>
+                <IonGrid className='root'>
+                    <div className='preview-box'>
+                        <img src={imageData}></img>
+                    </div>
+                </IonGrid>
             </IonContent>
+
+            <IonFab horizontal='center' vertical='bottom'>
+                <IonFabButton onClick={onClickDetectSiteImage}>
+                    <IonIcon icon={checkmarkOutline}></IonIcon>
+                </IonFabButton>
+            </IonFab>
+
         </IonPage>
+
+
     );
 };
 
